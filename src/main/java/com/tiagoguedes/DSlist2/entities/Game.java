bdb3ad789b1bfad1e583d081b2c5fa89023e17e1
Tiago Guedes <tiagogguedes@gmail.com>
2025-01-23 13:57:52 -0300

    Game adicionado

--- src/main/java/com/tiagoguedes/DSlist2/entities/Game.java
+++ src/main/java/com/tiagoguedes/DSlist2/entities/Game.java
@@ -10,32 +10,38 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_Game")
+@Table(name = "tb_game")
 public class Game {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
+	private Double score;
+
+
 	@Column(name = "game_year")
 	private Integer year;
+	
 	private String genre;
-	private String platform;
-	@Column(name = "game_score")
-	private Double score;
+	private String platforms;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	
-	public Game(Long id, String title, Integer year, String genre, String platform, Double score, String imgUrl,
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
@@ -83,13 +89,13 @@ public class Game {
 	}
 
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
 	}
 
 
