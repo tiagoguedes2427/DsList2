d1de4ec17448940da7793d08dcab15e9d185197e
Tiago Guedes <tiagogguedes@gmail.com>
2025-01-23 12:30:02 -0300

    Criado projeto

---
+++ src/main/java/com/tiagoguedes/DSlist2/DSlist2Application.java
@@ -0,0 +1,13 @@
+package com.tiagoguedes.DSlist2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSlist2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSlist2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoguedes/DSlist2/DSlist2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoguedes.DSlist2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DSlist2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
